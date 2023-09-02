f94c245aa236dad16af6a2a6ed235c41c2895f0c
Ahmbhq <143860460+Ahmbhq@users.noreply.github.com>
2023-09-02 11:04:19 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,12 @@
+men = input("ayam goreng")
+harga = 12.000
+menu = input("pecel lele")
+harga = 13.000
+menu = input("mie goreng")
+harga = 12.000
+print (menu)
+print(harga)
+print (menu)
+print(harga)
+print (menu)
+print(harga)
